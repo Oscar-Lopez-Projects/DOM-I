c4e8d864ce7d54863372d662cef1eda141580a98
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2019-04-10 20:10:46 -0700

    Finished Dom Assignment.

--- js/index.js
+++ js/index.js
@@ -36,7 +36,159 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
+//nav
+  const navA = document.querySelectorAll('a');
+    const values = Object.values(siteContent.nav);
+
+    for (let i = 0; i < navA.length; i++) {
+      navA[i].textContent = values[i];
+    }
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//--------H1
+const ctaH1 = document.querySelector('div.cta-text > h1');
+
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+//--------button
+
+const ctaButton = document.querySelector('div.cta-text > button');
+
+ctaButton.textContent = siteContent["cta"]["button"];
+
+//-------CTA Image
+
+const ctaImage = document.getElementById('cta-img');
+
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
+
+//-------------Features-h4
+
+const featTitle = document.querySelector('div.text-content > h4');
+
+featTitle.textContent = siteContent["main-content"]["features-h4"];
+
+//-------------features-content
+
+const featContent = document.querySelector('div.text-content > p');
+
+featContent.textContent = siteContent["main-content"]["features-content"];
+
+//-------------about-h4
+
+
+const aboutTitle = document.querySelector('div.text-content:nth-child(2) > h4');
+
+aboutTitle.textContent = siteContent["main-content"]["about-h4"];
+
+//-------------about-content
+
+const aboutCont = document.querySelector('div.text-content:nth-child(2) > p');
+
+aboutCont.textContent = siteContent["main-content"]["about-content"];
+
+
+//-------------middle-img-src
+
+const midImage = document.getElementById('middle-img');
+
+midImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//------------services-h4
+
+const servTitle = document.querySelector('div.bottom-content > div.text-content > h4');
+
+servTitle.textContent = siteContent["main-content"]["services-h4"];
+
+//------------services-content
+
+const servCont = document.querySelector('div.bottom-content > div.text-content > p');
+
+servCont.textContent = siteContent["main-content"]["services-content"];
+
+//------------product-h4
+
+const prodTitle = document.querySelector('div.bottom-content > div.text-content:nth-child(2) > h4');
+
+prodTitle.textContent = siteContent["main-content"]["product-h4"];
+
+//------------product-content
+
+const prodCont = document.querySelector('div.bottom-content > div.text-content:nth-child(2) > p');
+
+prodCont.textContent = siteContent["main-content"]["product-content"];
+
+//------------vision-h4
+
+const visionTitle = document.querySelector('div.bottom-content > div.text-content:nth-child(3) > h4');
+
+visionTitle.textContent = siteContent["main-content"]["product-h4"];
+
+//------------vision-content
+
+const visionCont = document.querySelector('div.bottom-content > div.text-content:nth-child(3) > p');
+
+visionCont.textContent = siteContent["main-content"]["vision-content"];
+
+//--------Contact-h4
+
+const contactTitle = document.querySelector('section.contact > h4');
+
+contactTitle.textContent = siteContent["contact"]["contact-h4"];
+
+//--------address
+//NEED TO FIGURE OUT HOW TO ADD THE BREAK IN THE ADDRESS
+const contAddress = document.querySelector('section.contact > p');
+
+contAddress.textContent = siteContent["contact"]["address"];
+
+//-------phone
+
+const contPhone = document.querySelector('section.contact > p:nth-child(3)');
+
+contPhone.textContent = siteContent["contact"]["phone"];
+
+//-------email
+
+const contEmail = document.querySelector('section.contact > p:nth-child(4)');
+
+contEmail.textContent = siteContent["contact"]["email"];
+
+//------copyright
+
+const fCopyright = document.querySelector('footer > p');
+
+fCopyright.textContent = siteContent["footer"]["copyright"];
+
+
+//----------------------------------------------------ADD NEW CONTENT TO NAV
+
+//-------------Append
+
+let navB = document.createElement('a');
+let newText = document.createTextNode('Idea');
+navB.appendChild(newText);
+
+let element = document.querySelector('nav');
+element.appendChild(navB);
+
+
+//-------------Prepend
+
+let navC = document.createElement('a');
+let newTextTwo = document.createTextNode('Great');
+navC.appendChild(newTextTwo);
+
+let elementTwo = document.querySelector('nav');
+elementTwo.prepend(navC);
+
+//----------------------------------------------Make NAV text Green
+
+const navSelect = document.getElementsByTagName('a');
+
+for (let i = 0; i < navSelect.length; i++) {
+  navSelect[i].style.color = 'green';
+}
